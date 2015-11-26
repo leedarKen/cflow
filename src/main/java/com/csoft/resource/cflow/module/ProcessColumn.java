e8154ccc2bc09d7030abccda60aa9ae4a5dec849
ken <348999853@qq.com>
2015-11-26 13:50:35 +0800

    update

--- src/main/java/com/csoft/resource/cflow/module/ProcessColumn.java
+++ src/main/java/com/csoft/resource/cflow/module/ProcessColumn.java
@@ -5,7 +5,7 @@ import com.csoft.resource.cflow.constants.ProcessColumnTypeConstant;
 /**
  * Created by ken.kang on 2015/11/24.
  */
-public class ProcessColumn {
+public abstract class ProcessColumn {
     private String id ;
     private ProcessColumnTypeConstant columnTypeConstant ;
     private String name ;
