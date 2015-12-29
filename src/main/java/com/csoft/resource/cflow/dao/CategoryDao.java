0f34383092290ffd312f43720756eab59823ec0f
ken <348999853@qq.com>
2015-12-29 14:25:31 +0800

    change mothod parameters to map and remove string paramete's method

--- src/main/java/com/csoft/resource/cflow/dao/CategoryDao.java
+++ src/main/java/com/csoft/resource/cflow/dao/CategoryDao.java
@@ -2,8 +2,9 @@ package com.csoft.resource.cflow.dao;
 
 import com.csoft.resource.cflow.pojo.Category;
 
-public interface CategoryDao extends GenericDao<Category,Integer>{
-	Category getCategory(String category, String type, String detailType);
+import java.util.Map;
 
-	Category getCategory(String category, String type);
+public interface CategoryDao extends GenericDao<Category, Integer> {
+
+    Category getCategory(Map<String, String> parameters);
 }
--- src/main/java/com/csoft/resource/cflow/dao/impl/CategoryDaoHibernate.java
+++ src/main/java/com/csoft/resource/cflow/dao/impl/CategoryDaoHibernate.java
@@ -4,49 +4,40 @@ import com.csoft.resource.cflow.dao.CategoryDao;
 import com.csoft.resource.cflow.dao.GenericDao;
 import com.csoft.resource.cflow.pojo.Category;
 import com.csoft.resource.cflow.pojo.InstanceTask;
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.classic.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository("categoryDao")
 public class CategoryDaoHibernate
         extends GenericDaoHibernate<Category, Integer> implements CategoryDao {
+
+    Logger logger = Logger.getLogger(CategoryDaoHibernate.class);
 
     public CategoryDaoHibernate() {
         super(Category.class);
     }
 
     @Override
-    public Category getCategory(String category, String type, String detailType) {
+    public Category getCategory(Map<String, String> parameters) {
         Session session = getSessionFactory().getCurrentSession();
         Criteria cri = session.createCriteria(Category.class);
-        cri.add(Restrictions.eq("category", category));
-        cri.add(Restrictions.eq("type", type));
-        cri.add(Restrictions.eq("detailType", detailType));
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            cri.add(Restrictions.eq(entry.getKey(), entry.getValue()));
+        }
+
         List<Category> categoryList = cri.list();
 
         if (categoryList != null && !categoryList.isEmpty()) {
+            logger.info("getCategory method end: category.id=" + categoryList.get(0).getId());
             return categoryList.get(0);
         }
-
-        return null;
-    }
-
-    @Override
-    public Category getCategory(String category, String type) {
-        Session session = getSessionFactory().getCurrentSession();
-        Criteria cri = session.createCriteria(Category.class);
-        cri.add(Restrictions.eq("category", category));
-        cri.add(Restrictions.eq("type", type));
-        List<Category> categoryList = cri.list();
-
-        if (categoryList != null && !categoryList.isEmpty()) {
-            return categoryList.get(0);
-        }
-
+        logger.info("getCategory method end: no Category found");
         return null;
     }
 }
